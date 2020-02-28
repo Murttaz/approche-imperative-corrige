bccab5058e96bf920e7d0ab7fbfacd0ad5e0cd23
Xavier CALLOCH <callochxavier@gmail.com>
2020-02-28 12:45:12 +0100

    1er commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,30 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 10;
+		short s = -17282;
+		int i = 2846325;
+		long l = 10000L;
+		float f = 2.5F;
+		double d = 6.4D;
+		char c = 'c';
+		boolean myBool = true;
+		
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(myBool);
+		
+		String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+		System.out.println(randomString);
+
+	}
+
+}
